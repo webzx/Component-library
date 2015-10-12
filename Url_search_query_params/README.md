490666aad744c678b2724570ce74c4abced150de
unknown <332818630@qq.com>
2015-10-13 00:44:32 +0800

    url search query params

---
+++ Url_search_query_params/README.md
@@ -0,0 +1,4 @@
+# 查询URL中search参数
+> 该组件支持*CMD模块化引用*和*普通引用*
+* `QueryString(key)` 查询指定参数值
+* `QueryStrings()` 查询所有参数，返回object键值对
---
+++ Url_search_query_params/main.js
@@ -0,0 +1,47 @@
+﻿(function (factory) {
+    if (typeof define === 'function' && define.cmd) {
+        // CMD，模块化代码
+        define(function (require, exports, module) {
+            return factory();
+        });
+    } else {
+        // 如果define没有被定义，正常执行插件代码。将方法封装在自定义命名空间 $.cPlugin中
+        $.cPlugin = $.cPlugin ? $.cPlugin : {};
+        $.extend($.cPlugin, factory());
+    }
+}(function () {
+
+    /**
+     * 获取URL中指定参数
+     * @param key 参数name
+     * @returns {*} 返回参数value
+     * @constructor
+     */
+    function QueryString(key) {
+        var uri = decodeURIComponent(window.location.search),
+            re = new RegExp("[\&\?]" + key + "\=([^\&]*)", "ig");
+        return ((uri.match(re)) ? (uri.match(re)[0].substr(key.length + 2)) : null);
+    }
+
+    /**
+     * 获取URL中所有参数
+     * @returns {{}} 返回object键值对 例如:{a_name:a_value,b_name:b_name}
+     * @constructor
+     */
+    function QueryStrings () {
+        var uri = decodeURIComponent(window.location.search),
+            re = /\w*\=([^\&]*)/ig,
+            params = {},
+            arr=[];
+        while ((arr = re.exec(uri)) != null) {
+            var temp = arr[0].split('=');
+            params[temp[0]] = temp[1];
+        }
+        return params;
+    }
+
+    return {
+        QueryString: QueryString,
+        QueryStrings:QueryStrings
+    }
+}));
